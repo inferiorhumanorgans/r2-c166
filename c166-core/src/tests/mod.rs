5f16be483d030569f01731abd788fa1bc5a67a8e
Alex Zepeda <alex@inferiorhumanorgans.com>
2018-05-16 20:57:08 -0700

    Refactor test macros into common file

--- c166-core/src/tests/mod.rs
+++ c166-core/src/tests/mod.rs
@@ -1,2 +1,6 @@
+#[macro_use]
+mod test_macros;
+
+pub mod trap;
 pub mod xor;
 pub mod xorb;
---
+++ c166-core/src/tests/test_macros.rs
@@ -0,0 +1,20 @@
+macro_rules! test_requires {
+    () => {
+        use ::instruction::Instruction;
+        use ::encoding::Encoding;
+        use opformat::OpFormat;
+    };
+}
+
+macro_rules! test_disasm_op {
+    ( $input:expr, $expected:expr ) => {
+        let bytes = $input;
+        let op = Instruction::from_addr_array(&bytes).unwrap();
+        let encoding = Encoding::from_encoding_type(&op.encoding).unwrap();
+        let format = OpFormat::from_format_type(&op.format).unwrap();
+        let values = (encoding.decode)(&bytes);
+        let desc = (format.decode)(&op, values);
+
+        assert_eq!(desc, $expected);
+    };
+}
