74b03d97e72402f4c128d586e41cc7ab01751b68
Alex Zepeda <alex@inferiorhumanorgans.com>
2018-05-25 13:45:48 -0700

    ESIL test updates.

--- c166-analysis/tests/common/mod.rs
+++ c166-analysis/tests/common/mod.rs
@@ -20,7 +20,7 @@ pub fn r2_eval_asm_op<'a>(op: &str, cmd: &str) -> String {
 }
 
 pub fn r2_eval_asm_op_with_init<'a>(op: &str, cmd: &str, init: &str) -> String {
-    let r2_cmd : String = format!("s 0; wx {op}; {init}; ds; {cmd}", op=op, cmd=cmd, init=init);
+    let r2_cmd : String = format!("s 0; wx {op}; {init}; ds 1; s 0; {cmd}", op=op, cmd=cmd, init=init);
     let ret : Cow<'a, str> = run_radare(&["-2", "-0", "-a", "c166", "-b", "16", "-Q", "-c", r2_cmd.as_str(), "-"]);
     let substr = ( ret[0..(ret.len() - 1)] ).to_string();
     eprintln!("R2: >> {}\nR2: << {}", r2_cmd, substr);
@@ -32,6 +32,11 @@ pub fn r2_eval_asm_op_reg(op: &str, reg: &str) -> String {
 }
 
 pub fn r2_eval_asm_op_reg_with_init(op: &str, reg: &str, init: &str) -> String {
-    let reg_cmd = format!("dr {reg}", reg=reg);
+    let reg_cmd = format!("ar {reg}", reg=reg);
+    r2_eval_asm_op_with_init(op, reg_cmd.as_str(), init)
+}
+
+pub fn r2_eval_asm_op_indirect_with_init(op: &str, reg: &str, init: &str) -> String {
+    let reg_cmd = format!("ae {reg},[]", reg=reg);
     r2_eval_asm_op_with_init(op, reg_cmd.as_str(), init)
 }
--- c166-analysis/tests/esil/mov.rs
+++ c166-analysis/tests/esil/mov.rs
@@ -22,12 +22,17 @@ fn esil_mov_a8() {
 
 #[test]
 fn esil_mov_98() {
-    assert_eq!(r2_eval_asm_op_reg_with_init("9872", "r7", "dr r2=0x8;s 8;wx 1234;s 0"), "0x00003412");
-    assert_eq!(r2_eval_asm_op_reg_with_init("9872", "r2", "dr r2=0x8;s 8;wx 1234;s 0"), "0x0000000a");
+    let instruction : &str = "9872";
+    let init_cmd : &str = "dr r2=0x8;s 8;wx 1234;s 0";
+    assert_eq!(r2_eval_asm_op_reg_with_init(instruction, "r7", init_cmd), "0x00003412");
+    assert_eq!(r2_eval_asm_op_reg_with_init(instruction, "r2", init_cmd), "0x0000000a");
 }
 
 #[test]
 fn esil_mov_b8() {
-    assert_eq!(r2_eval_asm_op_reg_with_init("9872", "r7", "dr r2=0x8;s 8;wx 1234;s 0"), "0x00003412");
-    assert_eq!(r2_eval_asm_op_reg_with_init("9872", "r2", "dr r2=0x8;s 8;wx 1234;s 0"), "0x0000000a");
+    // Write instruction 'B8 72' to 0x00
+    // Run setup r2 command 'dr r2=0x8;dr r7=0x1234'
+    // Dereference r2
+    // Hope it equals 0x1234
+    assert_eq!(r2_eval_asm_op_indirect_with_init("B872", "r2", "dr r2=0x8;dr r7=0x1234"), "0x1234");
 }
